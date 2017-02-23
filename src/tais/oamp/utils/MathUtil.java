69374828897da6359938c7f97f92b995745cafdc
nithi <nithunanda@gmail.com>
2017-02-23 10:19:17 -0800

    Oamp initial checkin

---
+++ src/tais/oamp/utils/MathUtil.java
@@ -0,0 +1,13 @@
+package tais.oamp.utils;
+
+public class MathUtil {
+	
+	static public int add(int a, int b) {
+		return a + b;
+	}
+	
+	static public int multiply ( int a, int b) {
+		return a * b;
+	}
+
+}
